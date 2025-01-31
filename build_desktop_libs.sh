9a3b8def13f8b37c3d66a93f27cceb616944db3b
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-01-31 13:44:49 +0900

    build desktop libraries locally

--- build_desktop_libs.sh
+++ build_desktop_libs.sh
@@ -5,9 +5,7 @@ mkdir tflite_build
 cd tflite_build
 
 # Build LiteRT C library
-cmake ../tensorflow_src/tensorflow/lite/c
+cmake ../tensorflow/tensorflow/lite \
+    -DTFLITE_ENABLE_GPU=ON \
+    -DTFLITE_ENABLE_RUY=ON
 cmake --build . -j
-
-cmake ../tensorflow_src/tensorflow/lite 
-#    -DTFLITE_ENABLE_GPU=ON \
-#    -DTFLITE_ENABLE_RUY=ON
