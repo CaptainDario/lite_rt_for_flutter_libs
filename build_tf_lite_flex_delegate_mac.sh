ced2498f38bbc47db61fdab22cafa9cf6cde122f
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-02 18:31:15 +0900

    updated build script

--- build_tf_lite_flex_delegate_mac.sh
+++ build_tf_lite_flex_delegate_mac.sh
@@ -4,7 +4,7 @@ bazelisk clean
 
 bazelisk build -c opt \
   --repo_env=HERMETIC_PYTHON_VERSION=3.11 \
-  //tensorflow/lite/delegates/flex:delegate
+  //tensorflow/lite/delegates/flex:tensorflowlite_flex
 
 find ./bazel-out/ -type f -name "*.dylib"
 
