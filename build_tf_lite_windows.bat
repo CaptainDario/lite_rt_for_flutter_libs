2c2ff270e84cc01fe5117261a073d9ac8f58b88d
Dario Windows <>
2025-02-04 05:43:47 -0800

    updated

--- build_tf_lite_windows.bat
+++ build_tf_lite_windows.bat
@@ -1,15 +1,11 @@
 cd tensorflow
 
-bazelisk clean
+bazel clean
 
-(
-  bazelisk build -c opt
-    --repo_env=HERMETIC_PYTHON_VERSION=3.11
-    //tensorflow/lite/c:tensorflowlite_c
-)
+bazel build -c opt --repo_env=HERMETIC_PYTHON_VERSION=3.11 //tensorflow/lite/c:tensorflowlite_c
 
-find ./bazel-out/ -type f -name "*.dll"
+# find ./bazel-out/ -type f -name "*.dll"
 
-cp $(find ./bazel-out/ -type f -name "*.dll") ./libtensorflowlite_arm64_c.dll
+# cp $(find ./bazel-out/ -type f -name "*.dll") ./libtensorflowlite_arm64_c.dll
 
 
