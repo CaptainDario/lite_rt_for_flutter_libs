7ec174605cf1424e48d76b98ac4f9cec58b1679f
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-04 16:34:43 +0900

    use brackets instead of line break

--- build_tf_lite_windows.bat
+++ build_tf_lite_windows.bat
@@ -2,9 +2,11 @@ cd tensorflow
 
 bazelisk clean
 
-bazelisk build -c opt \
-  --repo_env=HERMETIC_PYTHON_VERSION=3.11 \
-  //tensorflow/lite/c:tensorflowlite_c
+(
+  bazelisk build -c opt
+    --repo_env=HERMETIC_PYTHON_VERSION=3.11
+    //tensorflow/lite/c:tensorflowlite_c
+)
 
 find ./bazel-out/ -type f -name "*.dll"
 
