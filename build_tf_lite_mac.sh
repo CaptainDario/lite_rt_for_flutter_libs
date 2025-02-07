cced9077eeaf2cb43de110f51ca7197a26db2d74
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-07 15:29:35 +0900

    build x86 mac library

--- build_tf_lite_mac.sh
+++ build_tf_lite_mac.sh
@@ -1,11 +1,9 @@
 cd tensorflow
+arch -x86_64 bazelisk clean
 
-bazelisk clean
-
-bazelisk build -c opt \
+arch -x86_64 bazelisk build -c opt \
   --repo_env=HERMETIC_PYTHON_VERSION=3.11 \
   //tensorflow/lite/c:tensorflowlite_c
-
 find ./bazel-out/ -type f -name "*.dylib"
 
 cp $(find ./bazel-out/ -type f -name "*.dylib") ./libtensorflowlite_arm64.dylib
