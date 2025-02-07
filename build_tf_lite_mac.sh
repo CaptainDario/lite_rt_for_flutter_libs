03c5e426eee8ba6a7901d313fe2c8b747aa30c14
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-07 15:44:37 +0900

    updated mac and windows for arm support

--- build_tf_lite_mac.sh
+++ build_tf_lite_mac.sh
@@ -1,11 +1,16 @@
+echo $ARCH
+
+# clean tensorflow build directory
 cd tensorflow
 bazelisk clean
 
+# build base rnutime and check it was successful
 bazelisk build -c opt \
   --repo_env=HERMETIC_PYTHON_VERSION=3.11 \
   //tensorflow/lite/c:tensorflowlite_c
 find ./bazel-out/ -type f -name "*.dylib"
 
-cp $(find ./bazel-out/ -type f -name "*.dylib") ./libtensorflowlite_arm64.dylib
+# rename and copy the output based on the current system
+cp $(find ./bazel-out/ -type f -name "*.dylib") ./libtensorflowlite_${ARCH}_c.dylib
 
 
