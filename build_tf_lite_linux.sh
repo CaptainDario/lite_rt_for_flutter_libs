5d26d05b8066598e4146ff69ca3ed05df5ab5b91
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-07 13:04:48 +0900

    updated linux building to target arm

--- build_tf_lite_linux.sh
+++ build_tf_lite_linux.sh
@@ -1,13 +1,23 @@
+# clean tensorflow build directory
 cd tensorflow
-
 bazelisk clean
 
+# build base rnutime and check it was successful
 bazelisk build -c opt \
   --repo_env=HERMETIC_PYTHON_VERSION=3.11 \
   //tensorflow/lite/c:tensorflowlite_c
-
 find ./bazel-out/ -type f -name "*.so"
 
-cp $(find ./bazel-out/ -type f -name "*.so") ./libtensorflowlite_x86.so
+# rename and copy the output based on the current system
+if [[ "${RUNNER_ARCHITECTURE}" == "x64" ]]; then
+    arch="x86"
+elif [[ "${RUNNER_ARCHITECTURE}" == "arm64" ]]; then
+    arch="arm64"
+else
+    echo "Unsupported architecture: ${RUNNER_ARCHITECTURE}"
+    exit 1
+fi
+cp $(find ./bazel-out/ -type f -name "*.so") "./libtensorflowlite_${arch}_c.so"
+
 
 
