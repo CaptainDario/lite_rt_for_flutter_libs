90f15617bb6d45c86beca5e064552f04578b34b5
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-07 15:40:18 +0900

    try building on native x86 runner

--- build_tf_lite_mac.sh
+++ build_tf_lite_mac.sh
@@ -1,7 +1,7 @@
 cd tensorflow
-arch -x86_64 bazelisk clean
+bazelisk clean
 
-arch -x86_64 bazelisk build -c opt \
+bazelisk build -c opt \
   --repo_env=HERMETIC_PYTHON_VERSION=3.11 \
   //tensorflow/lite/c:tensorflowlite_c
 find ./bazel-out/ -type f -name "*.dylib"
