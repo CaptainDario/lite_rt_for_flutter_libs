b7b8dda1bdd18d2da297d95fc07d4219e40182f6
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-08 17:51:15 +0900

    gpu delegate plugin

---
+++ lite_rt_for_flutter_libs_gpu_delegate/ios/Classes/lite_rt_for_flutter_libs_gpu_delegate.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../lite_rt_for_flutter_libs_gpu_delegate.podspec for more information.
+#include "../../src/lite_rt_for_flutter_libs_gpu_delegate.c"
