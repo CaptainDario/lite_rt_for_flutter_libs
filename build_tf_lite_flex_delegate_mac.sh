792d28407ec7d7b2ceb5e0f3b88cbdc57bd64433
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-09 20:40:38 +0900

    build flex delegate monolithic

--- build_tf_lite_flex_delegate_mac.sh
+++ build_tf_lite_flex_delegate_mac.sh
@@ -2,7 +2,7 @@ cd tensorflow
 
 bazelisk clean
 
-bazelisk build -c opt \
+bazelisk build -c opt --config=monolithic \
   --repo_env=HERMETIC_PYTHON_VERSION=3.11 \
   //tensorflow/lite/delegates/flex:tensorflowlite_flex
 
@@ -10,3 +10,4 @@ find ./bazel-out/ -type f -name "*.dylib"
 
 cp $(find ./bazel-out/ -type f -name "*.dylib") ./libtensorflowlite_arm64_flex.dylib
 
+install_name_tool -id @rpath/libtensorflowlite_flex.dylib libtensorflowlite_arm64_flex.dylib
