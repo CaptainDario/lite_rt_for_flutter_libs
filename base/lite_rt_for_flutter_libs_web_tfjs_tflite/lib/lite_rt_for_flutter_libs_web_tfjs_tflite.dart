777883a504eb3aac0cd2bf5de7836d4079b8c53d
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-14 15:58:40 +0900

    removed unused files

--- base/lite_rt_for_flutter_libs_web_tfjs_tflite/lib/lite_rt_for_flutter_libs_web_tfjs_tflite.dart
+++
@@ -1,8 +0,0 @@
-
-import 'lite_rt_for_flutter_libs_web_tfjs_tflite_platform_interface.dart';
-
-class LiteRtForFlutterLibsWebTfjsTflite {
-  Future<String?> getPlatformVersion() {
-    return LiteRtForFlutterLibsWebTfjsTflitePlatform.instance.getPlatformVersion();
-  }
-}
