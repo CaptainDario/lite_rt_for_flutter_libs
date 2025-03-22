9d53a40faf903494ca233779e947d47d30a29694
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-03-22 20:02:42 +0100

    flex delegate linux package

---
+++ flex/lite_rt_for_flutter_libs_flex_delegate_linux/linux/include/lite_rt_for_flutter_libs_flex_delegate_linux/lite_rt_for_flutter_libs_flex_delegate_linux_plugin.h
@@ -0,0 +1,26 @@
+#ifndef FLUTTER_PLUGIN_LITE_RT_FOR_FLUTTER_LIBS_FLEX_DELEGATE_LINUX_PLUGIN_H_
+#define FLUTTER_PLUGIN_LITE_RT_FOR_FLUTTER_LIBS_FLEX_DELEGATE_LINUX_PLUGIN_H_
+
+#include <flutter_linux/flutter_linux.h>
+
+G_BEGIN_DECLS
+
+#ifdef FLUTTER_PLUGIN_IMPL
+#define FLUTTER_PLUGIN_EXPORT __attribute__((visibility("default")))
+#else
+#define FLUTTER_PLUGIN_EXPORT
+#endif
+
+typedef struct _LiteRtForFlutterLibsFlexDelegateLinuxPlugin LiteRtForFlutterLibsFlexDelegateLinuxPlugin;
+typedef struct {
+  GObjectClass parent_class;
+} LiteRtForFlutterLibsFlexDelegateLinuxPluginClass;
+
+FLUTTER_PLUGIN_EXPORT GType lite_rt_for_flutter_libs_flex_delegate_linux_plugin_get_type();
+
+FLUTTER_PLUGIN_EXPORT void lite_rt_for_flutter_libs_flex_delegate_linux_plugin_register_with_registrar(
+    FlPluginRegistrar* registrar);
+
+G_END_DECLS
+
+#endif  // FLUTTER_PLUGIN_LITE_RT_FOR_FLUTTER_LIBS_FLEX_DELEGATE_LINUX_PLUGIN_H_
