df40ab2ee9e6f82a270f63bab902197c4d595f3c
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-03-22 20:11:26 +0100

    added gpu android package

---
+++ gpu/lite_rt_for_flutter_libs_gpu_delegate_android/android/src/main/kotlin/com/example/lite_rt_for_flutter_libs_gpu_delegate_android/LiteRtForFlutterLibsGpuDelegateAndroidPlugin.kt
@@ -0,0 +1,33 @@
+package com.example.lite_rt_for_flutter_libs_gpu_delegate_android
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+
+/** LiteRtForFlutterLibsGpuDelegateAndroidPlugin */
+class LiteRtForFlutterLibsGpuDelegateAndroidPlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "lite_rt_for_flutter_libs_gpu_delegate_android")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+}
