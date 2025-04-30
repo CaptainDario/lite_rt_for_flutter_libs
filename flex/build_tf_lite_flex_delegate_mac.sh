488129bba533119587a1dec11ca5fc3e31caaa29
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-04-30 22:19:32 +0200

    updated macos build

--- flex/build_tf_lite_flex_delegate_mac.sh
+++ flex/build_tf_lite_flex_delegate_mac.sh
@@ -8,6 +8,6 @@ bazelisk build -c opt --config=monolithic \
 
 find ./bazel-out/ -type f -name "*.dylib"
 
-cp $(find ./bazel-out/ -type f -name "*.dylib") ./libtensorflowlite_${ARCH}_flex.dylib
+cp $(find ./bazel-out/ -type f -name "*.dylib") ./libtensorflowlite_${ARCH}_flex_delegate_c.dylib
 
-install_name_tool -id @rpath/libtensorflowlite_flex.dylib libtensorflowlite_${ARCH}_flex.dylib
+install_name_tool -id @rpath/libtensorflowlite_flex_delegate_c.dylib libtensorflowlite_${ARCH}_flex_delegate_c.dylib
