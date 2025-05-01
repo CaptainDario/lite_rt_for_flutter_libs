935c7ab64bea0ca6378d7132d6b15ad79d89b2a9
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-05-01 17:21:47 +0200

    testing gpu delegate

--- gpu/build_tf_lite_gpu_delegate_windows.sh
+++ gpu/build_tf_lite_gpu_delegate_windows.sh
@@ -4,7 +4,7 @@ mkdir tflite_build
 cd tflite_build
 
 
-cmake ../tensorflow_src/tensorflow/lite/c -DTFLITE_ENABLE_GPU=ON -DCMAKE_BUILD_TYPE=release
+cmake ../tensorflow/tensorflow/lite/c -DTFLITE_ENABLE_GPU=ON -DCMAKE_BUILD_TYPE=release
 cmake --build . -j 8 --config release
 
 ls -la
