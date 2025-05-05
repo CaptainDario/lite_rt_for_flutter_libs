cbdfbfa9924d61fd619848d762ec6c129cd4ddb1
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-05-05 16:58:44 +0200

    building tf lite

--- flex/build_tf_lite_flex_delegate_windows.sh
+++ flex/build_tf_lite_flex_delegate_windows.sh
@@ -2,10 +2,13 @@ cd tensorflow
 
 bazelisk clean
 
-bazelisk build -c opt --config=monolithic \
+bazel build -c opt \ 
   --repo_env=HERMETIC_PYTHON_VERSION=3.11 \
-  --cxxopt='/std:c++20' \
   tensorflow/lite/delegates/flex:tensorflowlite_flex
+#bazelisk build -c opt --config=monolithic \
+#  --repo_env=HERMETIC_PYTHON_VERSION=3.11 \
+#  --cxxopt='/std:c++20' \
+#  tensorflow/lite/delegates/flex:tensorflowlite_flex
   #--config=win_clang \
 
 find ./bazel-out/ -type f -name "*.dll"
