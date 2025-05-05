2c82eda5d0637b2de32d0d69f71d1aadd27b307e
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-05-05 17:46:05 +0200

    building

--- flex/build_tf_lite_flex_delegate_windows.sh
+++ flex/build_tf_lite_flex_delegate_windows.sh
@@ -3,6 +3,7 @@ cd tensorflow
 bazelisk clean
 
 bazel build -c opt \ 
+  --config=monolithic \
   --repo_env=HERMETIC_PYTHON_VERSION=3.11 \
   tensorflow/lite/delegates/flex:tensorflowlite_flex
 #bazelisk build -c opt --config=monolithic \
